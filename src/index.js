2352deb7ed7597ad311b42cbcc7abf0db500d460
Arcadie <arcadie2klearn@gmail.com>
2022-04-25 19:42:03 +0300

    fixed dotenv issue

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
 import "flowbite";
-require("dotenv").config();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
